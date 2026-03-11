f991f4429a95877f940760dd5ff02e2f6b19c72a
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-03-11 16:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	size = Number(size)
+	if (str.length === 0) return []
+	
+	let res = []; //["Hello", ", wor", "ld!"] 
+	let i = 0; // 10
+
+	while (i < str.length){
+		let newStr = str.slice(i, i+size); // 10, 10+5=15
+		res.push(newStr);
+		i += size;
+	}
+
+	return res;
 }
 
 // Do not change the code below
